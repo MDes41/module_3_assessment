167ce1204baea8cb71e7cbfdcf7f0e7eed37fcf0
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-11-02 10:09:50 -0600

    Starting to refactor

--- spec/feature/search_by_zip_code_returns_stores_spec.rb
+++ spec/feature/search_by_zip_code_returns_stores_spec.rb
@@ -18,9 +18,14 @@ RSpec.describe 'When I search by zip' do
 
     # And I should see the long name, city, distance, phone number and store type for each of the 15 results
     expect(page).to have_content('Name:')
+    expect(page).to have_content('Best Buy Mobile')
     expect(page).to have_content('City:')
+    expect(page).to have_content('Denver')
     expect(page).to have_content('Distance:')
+    expect(page).to have_content('3.25')
     expect(page).to have_content('Phone Number:')
+    expect(page).to have_content('303-270-9189')
     expect(page).to have_content('Store Type:')
+    expect(page).to have_content('Mobile')
   end
 end
