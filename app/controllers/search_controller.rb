278d99104f235319f906f9bfa1675eb6e3c6dc38
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-11-02 10:18:07 -0600

    Moved faraday to model stores

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,18 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,distance,city,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_BUY_KEY']}") do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
+    response = Store.get_requested_stores(search_params[:search])
 
-    response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
-    end
-    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,distance,city,phone,storeType&apiKey=#{ENV['BEST_BUY_KEY']}")
     json_parsed_response = JSON.parse(response.body, symbolize_names: true)
+
     @total = json_parsed_response[:total]
     @stores = json_parsed_response[:stores].map do |store_data|
       Store.new(store_data)
     end
     # byebug
   end
+
+  private
+    def search_params
+      params.require(:search).permit(:search)
+    end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -7,4 +7,16 @@ class Store
     @phone = store_data[:phone]
     @type = store_data[:storeType]
   end
+
+  def self.get_requested_stores(zip)
+    conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,distance,city,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_BUY_KEY']}") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+
+    response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
+    end
+  end
+
+  # json_parsed_response = JSON.parse(response.body, symbolize_names: true)
 end
