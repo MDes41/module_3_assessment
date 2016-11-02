e11205fadfca3390bd2bdd7bed9f248d8c5b4deb
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-11-02 11:30:38 -0600

    Going to skip status codes for now spending too much time

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -11,8 +11,39 @@ RSpec.describe 'Items API Controller' do
 
     json = JSON.parse(response.body)
 
-    # expect(response.status).to eq(200)
-    save_and_open_page
-    expect(page).to have_content()
+    expect(response.status).to eq(200)
+    expect(json.length).to eq(10)
+    expect(json.class).to eq(Array)
+  end
+
+  it 'can return info for one item' do
+
+    item = create :item
+    10.times do
+      create :item
+    end
+
+    get "/api/v1/items/#{item.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(json.class).to_not eq(Array)
+  end
+
+  it 'can delete one item with requested params' do
+
+    item = create :item
+    10.times do
+      create :item
+    end
+
+    delete "/api/v1/items/#{item.id}"
+
+    json = JSON.parse(response.body)
+
+    expect(response.status).to eq(204)
+    expect(json.class).to eq(Array)
+    expect(json.length).to eq(10)
   end
 end
