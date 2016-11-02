1f69d7c7f42bd71726d3cd2e6a66206d5bc976ee
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-11-02 10:37:50 -0600

    going to move service into its own model

--- app/models/store.rb
+++ app/models/store.rb
@@ -8,7 +8,7 @@ class Store
     @type = store_data[:storeType]
   end
 
-  def get_requested_data(zip)
+  def self.get_requested_data(zip)
     conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,distance,city,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_BUY_KEY']}") do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
@@ -27,17 +27,6 @@ class Store
   end
 
   def self.get_total_stores(zip)
-    conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(#{zip},25))?format=json&show=longName,distance,city,phone,storeType&pageSize=15&apiKey=#{ENV['BEST_BUY_KEY']}") do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-
-    response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
-    end
-
-    json_parsed_response = JSON.parse(response.body, symbolize_names: true)
-
-    @total = json_parsed_response[:total]
+    get_requested_data(zip)[:total]
   end
-
 end
