c838106efa420f9986b9286b9e1577b1e28b8dbd
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-11-02 09:48:36 -0600

    Working to get api through Faraday

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,distance,city,phone,storeType&apiKey=#{ENV['BEST_BUY_KEY']}") do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
 
+    response = conn.get do |req|                           # GET http://sushi.com/search?page=2&limit=100
+    end
+    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=longName,distance,city,phone,storeType&apiKey=#{ENV['BEST_BUY_KEY']}")
+    json_parsed_response = JSON.parse(response.body, symbolize_names: true)
+    byebug
   end
 end
